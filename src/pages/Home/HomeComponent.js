80ae3c6982cd0df9ef81b0b671fd0770bb24c822
dhanushkakasun <dhanushkakasun@gmail.com>
2022-06-04 20:50:07 +0530

    react- navigation done

--- src/pages/Home/HomeComponent.js
+++ src/pages/Home/HomeComponent.js
@@ -32,7 +32,7 @@ function HomeComponent() {
                                 <tr>
                                     <th scope="col">Train Number</th>
                                     <th scope="col">Train Name</th>
-                                    <th scope="col">from</th>
+                                    <th scope="col">From</th>
                                     <th scope="col">To</th>
                                     <th scope="col">Departure</th>
                                     <th scope="col">Arrival</th>
@@ -63,7 +63,7 @@ function HomeComponent() {
                     </div>
                     <div className="col">
 
-                        <img className="hc-image1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/%D0%9F%D0%BE%D0%B5%D0%B7%D0%B4_%D0%BD%D0%B0_%D1%84%D0%BE%D0%BD%D0%B5_%D0%B3%D0%BE%D1%80%D1%8B_%D0%A8%D0%B0%D1%82%D1%80%D0%B8%D1%89%D0%B5._%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6%D1%81%D0%BA%D0%B0%D1%8F_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C.jpg/1200px-%D0%9F%D0%BE%D0%B5%D0%B7%D0%B4_%D0%BD%D0%B0_%D1%84%D0%BE%D0%BD%D0%B5_%D0%B3%D0%BE%D1%80%D1%8B_%D0%A8%D0%B0%D1%82%D1%80%D0%B8%D1%89%D0%B5._%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6%D1%81%D0%BA%D0%B0%D1%8F_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C.jpg" alt="" />
+                        <img className="hc-image1" src="https://static.toiimg.com/photo/87870798.cms" alt="" />
                     </div>
                 </div>
             </div>
